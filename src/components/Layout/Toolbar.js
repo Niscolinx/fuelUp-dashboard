e2d66f5b918ae748780804fb54e3f812afeb3002
Igboanugwo Collins <munisco12@gmail.com>
2020-11-17 11:55:52 +0100

    working on the nav bar

--- src/components/Layout/Toolbar.js
+++ src/components/Layout/Toolbar.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import {BsFillGearFill} from 'react-icons/bs'
+import {IoMdNotifications} from 'react-icons/io'
+
 import Logo from './Logo'
+import avatar from '../../assets/images/avatar/avatar.jpg'
 import NavigationItems from './Navigation/NavigationItems'
 import SideDrawerToggle from './Navigation/SideDrawerToggle'
 
@@ -10,6 +14,13 @@ const toolbar = (props) => (
         <nav className='desktopOnly'>
             <NavigationItems auth={props.auth} logOut={props.logOut} />
         </nav>
+
+        <div>
+            <BsFillGearFill/>
+            <IoMdNotifications/>
+
+            <img src={avatar} alt='avatar'/>
+        </div>
     </div>
 )
 
