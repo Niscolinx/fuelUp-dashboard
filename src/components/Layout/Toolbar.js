b54bb340b6b2d4ae3385b2493ac7bf84012cf22a
Igboanugwo Collins <munisco12@gmail.com>
2020-11-17 19:56:19 +0100

    Working on the header

--- src/components/Layout/Toolbar.js
+++ src/components/Layout/Toolbar.js
@@ -8,14 +8,17 @@ import NavigationItems from './Navigation/NavigationItems'
 
 const toolbar = (props) => (
     <div className='toolbar'>
+        <div className='toolbar__1'>
         <Logo />
-        <nav className='toolbar__nav'>
+
+        </div>
+        <nav className='toolbar__2'>
             <NavigationItems auth={props.auth} logOut={props.logOut} />
         </nav>
 
-        <div>
-            <BsFillGearFill/>
-            <IoMdNotifications/>
+        <div className='toolbar__3'>
+            <IoMdNotifications className='toolbar__notification'/>
+            <BsFillGearFill className='toolbar__gear'/>
 
             <img src={avatar} alt='avatar' className='toolbar__avatar'/>
         </div>
